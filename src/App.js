527e458714767fd17cf3d4690d319e410384dd1e
Glen1986 <escobar.glen@gmail.com>
2022-05-04 14:53:33 -0300

    multiple-Context-classComponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { Component, createContext, useContext } from 'react'
+import { Component, createContext } from 'react'
 
-const Context = createContext('mi-valor')
+const Context1 = createContext('mi-valor')
+const Context2 = createContext('mi-valor2')
 
 const Provider = ({ children }) => {
-    return <Context.Provider value="otro-valor">{children}</Context.Provider>
+    return (
+        <Context1.Provider value="valor-1">
+            <Context2.Provider value="valor-2">{children}</Context2.Provider>
+        </Context1.Provider>
+    )
 }
 class Componente extends Component {
     // static contextType = Context
     render() {
         console.log(this.context)
-        return <div>weed</div>
+        return (
+            <Context1.Consumer>
+                {(valor1) => (
+                    <Context2.Consumer>
+                        {(valor2) => <div>{`${valor1} ${valor2}`}</div>}
+                    </Context2.Consumer>
+                )}
+            </Context1.Consumer>
+        )
     }
 }
-Componente.contextType = Context
 
 const App = () => {
     return (
         <Provider>
             <Componente />
-            <Context.Consumer>{(valor) => <div>{valor}</div>}</Context.Consumer>
         </Provider>
     )
 }
